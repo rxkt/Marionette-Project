3136afabea1a8a92c94eaa710245ab40fa324011
Jerry Dai <jdai315@gmail.com>
2015-05-03 21:50:11 -0400

    updates from class

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import flask, render_template , session , redirect , request
+from flask import Flask, render_template , session , redirect , request
+from flask.ext.socketio import SocketIO, emit
+"""
+
+from firebase import firebase
+firebase = firebase.FirebaseApplication('https://boiling-heat-3848.firebaseio.com',None)
+
+"""
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hello'
+socketio = SocketIO(app)
 
 @app.route("/")
-def home():
+@socketio.on('login')
+def home(email=None, password=None):
     if 'username' in session:
+        print('test: ' + str(email) + "  " + str(password))
         return render_template("test.html")
     else:
+        print('test: ' + str(email) + "  " + str(password))
         return render_template("test.html")
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host="0.0.0.0" , port = 8000)
+    socketio.run(app)
+    
