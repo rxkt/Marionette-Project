9b9e3fafb131740d891d0a310871c048b56d033f
Michael Huang <mikester591@gmail.com>
2015-05-24 21:52:23 -0400

    Functions

--- app.py
+++ app.py
@@ -12,55 +12,89 @@ def authenticate(func):
     def inner():
         if 'username'in session:
             return func()
-else:
-    return redirect('/login')
+        else:
+            return redirect('/login')
+
 
 @app.route("/login",methods=['GET','POST'])
+
 def login():
-    if request.method== 'GET':
+    if request.method== 'POST':
+        
+        
+        button = request.form['button']
+        username = request.form['username']
+        password = request.form['password']
+        
+        if button == 'Submit':
+            criteria = {'username': username, 'password': password}
+            user = db.find_user(criteria)
+            if user!=None and username!= "":
+                session['username'] = username
+                print session['username']
+            
+                return render_template('login.html',user=session['username'])
+            
+            else:
+                return render_template('login.html',error="wrong info")
+        else:
+            return redirect('/register')
+    else:
+        if 'username' in session:
+            return render_template('login.html',error='Insession')
         return render_template('login.html')
-    button = request.form['button']
-    username = request.form['username']
-    password = request.form['password']
-    
-    if button == 'cancel':
-        return redirect('/login')
-else:
-    criteria = {'username': username, 'password': password}
-    user = db.find_user(criteria)
-    if user:
-        session['username'] = username
-
-        return redirect('/')
-else:
-    return render_template('test.html')
-
+        
 @app.route('/register', methods=['GET', 'POST'])
 
 def register():
     if request.method == 'GET':
+        
         return render_template('register.html')
     button = request.form['button']
     username = request.form['username']
     password = request.form['password']
     first = request.form['first']
     last = request.form['last']
-    if button == 'cancel':
-        return redirect('/register')
+    if button == 'Cancel':
+        return redirect('/login')
     else:
         if not password or not first or not last:
             return render_template('register.html',error='incomplete')
         criteria = {'username': username}
-        if db.find_user(criteria):
+        if db.find_user({'username':username}):
             return render_template('register.html',error='username taken')
         else:
             
             user_params = {'username': username, 'password': password, 'first': first, 'last': last}
             db.new_user(user_params)
             
-            return render_template('login.html',error="Created")
+            return render_template('login.html',error="created")
 
-
+@authenticate 
+@app.route("/home")
+def home():
+  return render_template('home.html')
+@authenticate 
+@app.route("/upload",methods=['GET','POST'])
+def upload():
+    if request.method == 'GET':
+        
+        return render_template('upload.html')
+    button = request.form['button']
+    name= request.form['name']
+    category= request.form['category']
+    desc= request.form['desc']
+    quan= request.form['quantity']
+    cond= request.form['cond']
+    price=request.form['price']
+    if not price or not desc or not quan:
+        return render_template('upload.html',error=True)
+    else:
+        item_params={'name':name,'category':category,'desc':desc,'quantity':quan,'cond':cond,'price':price}
+        db.new_item(item_params)
+        print('Added new item')
+        return redirect('/upload')
+    
 if __name__ == "__main__":
     app.secret_key='B&S'
     app.debug = True
