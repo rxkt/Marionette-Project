c3a395253205eeb257b07384c5edd7b3b16a165b
rxkt <ericliao10797@hotmail.com>
2015-05-28 11:30:18 -0400

    P

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 #copies app.py to a new file, replaces app.run() to include host and runs it
 #therefore editing app.py would not affect the server
 #and we can develop on our end efficiently
+
+cd ~/Softdev2-Final
 file="server.py"
 
 #---------------------------------------------
@@ -10,14 +12,14 @@ search="app.run()"
 new="app.run(host='0.0.0.0',port=5000)"
 #-------------------------
 # copy the new file to run
-echo "copying file..."
+echo "copying app.py to server.py..."
 #we sleep to make it look fancy
-sleep 1
+sleep 0.3
 cp app.py $file
 #------------------
 # apply replacement
-echo "replacing search"
-sleep 1
+echo "replacing app.run() with app.run(host='0.0.0.0',port=5000)..."
+sleep 0.3
 for each in $(fgrep -l $search $file)
 do
     ex $each <<EOF
@@ -28,5 +30,5 @@ done
 #--------------
 #run the server
 echo "running "$file
-sleep 1
+sleep 0.3
 python $file
